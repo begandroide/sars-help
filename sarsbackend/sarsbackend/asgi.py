f8301263c58fb0bb12623921519d14e04bdda6e2
benjamin_gautier <benjamin.gautier@licklider.cl>
2020-05-17 20:36:39 -0400

    Sars help api login

--- sarsbackend/sarsbackend/asgi.py
+++ sarsbackend/sarsbackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sarshelpapi project.
+ASGI config for sarsbackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarshelpapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarsbackend.settings')
 
 application = get_asgi_application()
