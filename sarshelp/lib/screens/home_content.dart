1d961f5ebda4b7319b49c65c2c7ba72392fd7f39
begandroide <benjamin.gautier.14@sansano.usm.cl>
2020-06-09 18:09:06 -0400

    Vista de home content es modificada para mostrar info requerida, queda pendiente hacer todo el detalle dentro de los cards y distintas ventanas cuando presiona un botón.

--- sarshelp/lib/screens/home_content.dart
+++ sarshelp/lib/screens/home_content.dart
@@ -2,89 +2,100 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  BuildContext context;
+
   @override
   Widget build(BuildContext context) {
-    return showHomeContent(context);
+    this.context = context;
+    return showHomeContent();
   }
 
-}
+  Widget showHomeContent() {
+    return Center(
+        child: Column(children: <Widget>[
+          _getWelcomeHeader(),
+          _getVoluntiersCard(),
+          _getGeneralStatistics()
+    
+        ],) 
+      );
+  }
 
-Widget showHomeContent(BuildContext context) {
-  return Center(
-      child: Column(children: <Widget>[
-        Container(
-          alignment: Alignment.centerLeft,
-          height: 50.0,
-          margin: EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
-          child:RichText(
-            text: TextSpan(
-              text:'Bienvenid@ ',
-              style: DefaultTextStyle.of(context).style,
-              children: <TextSpan>[
-                TextSpan(text: 'Benjamín', style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: ' !')
-              ]
-            )
-          ),
+  Widget _getWelcomeHeader() {
+    return 
+      Container(
+        alignment: Alignment.centerLeft,
+        height: 50.0,
+        margin: EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+        child:RichText(
+          text: TextSpan(
+            text:'Bienvenid@ ',
+            style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              TextSpan(text: 'Benjamín', style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: ' !')
+            ]
+          )
         ),
-        Card(
+      );
+  }
+
+  Widget _getVoluntiersCard() {
+    return
+      Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.people),
+              contentPadding: EdgeInsets.only(left: 15.0,right: 40.0, top:10.0),
+              title: Text(
+                    'Voluntarios',
+                ),
+              subtitle: Text('Los voluntarios asisten a los que necesitan de ayuda ¡Únete al programa!'),
+              isThreeLine: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: const Text('SER VOLUNTARIO'),
+                  onPressed: () {/* ... */},
+                ),
+                FlatButton(
+                  child: const Text('MÁS INFO'),
+                  onPressed: () {/* ... */},
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+  }
+
+  Widget _getGeneralStatistics() {
+    return
+      SizedBox(
+        height: 400,
+        child: Card(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               const ListTile(
-                leading: Icon(Icons.people),
-                contentPadding: EdgeInsets.only(left: 15.0,right: 40.0, top:10.0),
-                title: Padding(
-                  padding: EdgeInsets.only(bottom:5),
-                  child: 
-                    Text(
-                      'Voluntarios',
-                      ),
-                  ),
-                subtitle: Text('Los voluntarios asisten a los que necesitan de ayuda.'),
-              ),
-              ButtonBar(
-                children: <Widget>[
-                  FlatButton(
-                    child: const Text('SER VOLUNTARIO'),
-                    onPressed: () {/* ... */},
-                  ),
-                  FlatButton(
-                    child: const Text('MÁS INFO'),
-                    onPressed: () {/* ... */},
-                  ),
-                ],
-              ),
+                leading: Icon(Icons.trending_up),
+                title: Text('Estadísticas'),
+                subtitle: Text('Resumen del estado actual de las alertas'),
+                isThreeLine: true,
+              )
             ],
           ),
-        ),
-        Card(
-
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const ListTile(
-                leading: Icon(Icons.album),
-                title: Text('The Enchanted Nightingale'),
-                subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-              ),
-              ButtonBar(
-                children: <Widget>[
-                  FlatButton(
-                    child: const Text('BUY TICKETS'),
-                    onPressed: () {/* ... */},
-                  ),
-                  FlatButton(
-                    child: const Text('LISTEN'),
-                    onPressed: () {/* ... */},
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-  
-      ],) 
-    );
+        )
+      );
+  }
 }
+
