b2afa6ba59c4d03a06cba08cfe6ddcc84fb6dcaa
begandroide <benjamin.gautier.14@sansano.usm.cl>
2020-06-10 00:06:25 -0400

    Se crea una mejor vista para el listado de solicitudes

--- sarshelp/lib/screens/create_request.dart
+++ sarshelp/lib/screens/create_request.dart
@@ -1,13 +1,23 @@
 
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class CreateRequestPage extends StatelessWidget {
+class CreateRequestPage extends StatefulWidget {
+  @override
+  _CreateRequestPageState createState() => _CreateRequestPageState();
+}
+
+class _CreateRequestPageState extends State<CreateRequestPage> {
   final _formKey = GlobalKey<FormState>();
+
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool isProcessingData = false;
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +49,19 @@ class CreateRequestPage extends StatelessWidget {
                 ], // Only numbers can be entered
               ),
               TextFormField(
+                maxLength: 40,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'El nombre corto es un campo requerido';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nombre corto *'
+                ),
+              ),
+              TextFormField(
                 maxLines: 5,
                 maxLength: 500,
                 decoration: InputDecoration(
@@ -50,16 +73,32 @@ class CreateRequestPage extends StatelessWidget {
               RaisedButton(
                 onPressed: () {
                   if (_formKey.currentState.validate()) {
+
                       final snackBar = SnackBar(content: Text('Procesando datos...'));
                       _scaffoldKey.currentState.showSnackBar(snackBar);
+                      FirebaseAuth.instance.currentUser().then((value) => {
+                        // Firestore.instance.collection('HelpRequests').document(value.uid).setData({})
+
+                      });
                     }
                   },
-                  child: Text('Enviar'),
+                  child: _buttonSave()
               ),
+              _buttonSave()
             ]
       )) 
 ),
     );
   }
-  
+
+  _buttonSave() {
+    Widget contentButton;
+    if (this.isProcessingData){
+      contentButton = CircularProgressIndicator();
+    } else {
+      contentButton = Text('Enviar'); 
+    }
+    return contentButton;
+  }
+
 }
--- sarshelp/lib/screens/requests_page.dart
+++ sarshelp/lib/screens/requests_page.dart
@@ -9,12 +9,19 @@ import 'package:sarshelp/widgets/common.dart';
 final dbRef = Firestore.instance.collection("HelpRequests");
 
 
-class RequestsPage extends StatelessWidget {
+class RequestsPage extends StatefulWidget {
+  @override
+  _RequestsPageState createState() => _RequestsPageState();
+}
+
+class _RequestsPageState extends State<RequestsPage> {
+  BuildContext context;
+
   @override
   Widget build(BuildContext context) {
     return 
     Scaffold(
-      body: buildRequestsPage(context),
+      body: buildRequestsPage(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Add your onPressed code here!
@@ -32,44 +39,79 @@ class RequestsPage extends StatelessWidget {
       MaterialPageRoute(builder: (context) => CreateRequestPage()),
     );
   }
-}
 
-Widget buildRequestsPage(BuildContext context) {
-  return FutureBuilder(
-    future: FirebaseAuth.instance.currentUser(),
-    builder: (context, AsyncSnapshot<FirebaseUser> user){
-      if(user.hasData) {
-        return FutureBuilder(
-          future: dbRef.document(user.data.uid).get(),
-          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot){
-            if(snapshot.hasData){
-            return StreamBuilder<QuerySnapshot>(
-              stream: dbRef.snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError)
-                  return new Text('Error: ${snapshot.error}');
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting: return buildWaitingScreen();
-                  default:
-                    return new ListView(
-                      children: snapshot.data.documents.map((DocumentSnapshot document) {
-                        return new ListTile(
-                          title: new Text(document['description'].toString()),
-                          subtitle: new Text(document['grade'].toString()),
+  Widget buildRequestsPage() {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (context, AsyncSnapshot<FirebaseUser> user){
+        if(user.hasData) {
+          return FutureBuilder(
+            future: dbRef.document(user.data.uid).collection('userRequests').getDocuments(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
+              if(snapshot.hasData){
+              return StreamBuilder<QuerySnapshot>(
+                stream: dbRef.document(user.data.uid).collection('userRequests').snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError)
+                    return new Text('Error: ${snapshot.error}');
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting: return buildWaitingScreen();
+                    default:
+                      return new ListView(
+                        padding: EdgeInsets.all(8),
+                        children: snapshot.data.documents.map((DocumentSnapshot document) {
+                          return new Card(
+                            child: Container(
+                              child: Column(children: <Widget>[
+                                ListTile(
+                                  leading: _getLeadingListTile(document),
+                                  title: new Text(document['shortName'].toString()),
+                                  subtitle: _getSubtitleListTile(document),
+                                  trailing: new Icon(Icons.more_vert),
+                                  isThreeLine: true,
+                                ),
+                                Row(children: <Widget>[
+                                ],)
+                              ],),),
 
-                        );
-                      }).toList(),
-                    );
-                }
-              },
-            );
-            } else {
-              return buildWaitingScreen();
+                          );
+                        }).toList(),
+                      );
+                  }
+                },
+              );
+              } else {
+                return buildWaitingScreen();
+              }
             }
-          }
-        );
-      } else {
-        return buildWaitingScreen();
-      }
-    });
+          );
+        } else {
+          return buildWaitingScreen();
+        }
+      });
+  }
+
+  _getLeadingListTile(DocumentSnapshot document) {
+    if(document['isClosed']) {
+      return new Icon(Icons.check);
+    } return new Icon(Icons.error); 
+  }
+
+  _getSubtitleListTile(DocumentSnapshot document) {
+    return 
+    Container(
+      padding: EdgeInsets.only(top: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text(DateTime.fromMillisecondsSinceEpoch(document['datetime'].millisecondsSinceEpoch).toString()),
+          Chip(
+            label: Text(document['grade'].toString(), style: TextStyle(color: Colors.white)),
+            backgroundColor: Colors.pink,
+          ),
+      ]
+    )
+    );
+  }
 }
