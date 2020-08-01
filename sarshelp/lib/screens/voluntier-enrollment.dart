61acc5392749800c5178268d5ad87a44afce8680
begandroide <benjamin.gautier.14@sansano.usm.cl>
2020-08-01 18:27:45 -0400

    :sparkler: Se termina vista de "ser voluntario" en el home de la aplicación; Se crea un widget que se pone sobre el stack de la navegación tal de que luego se pueda volver al home (widget normal).

--- sarshelp/lib/screens/voluntier-enrollment.dart
+++ sarshelp/lib/screens/voluntier-enrollment.dart
@@ -13,36 +13,104 @@ class VoluntierEnrollmentPage extends StatefulWidget {
 
 class _VoluntierEnrollmentPageState extends State<VoluntierEnrollmentPage> {
   final _formKey = GlobalKey<FormState>();
-
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   bool isProcessingData = false;
+  final String beVoluntierTitle = "Ser voluntario";
+  final String voluntierMessage =
+      "Los voluntarios ayudan a la comunidad atendiento las solicitudes de ayuda dentro de su misma comunidad.";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
-          title: Text('Ser voluntario'),
+          title: Text(beVoluntierTitle),
         ),
         body: SingleChildScrollView(
             child: Form(
                 key: _formKey,
                 child: Card(
-                  child: Column(children: <Widget>[
-                    Text("Los voluntarios nos ayudan a atender las solicitudes de ayuda"),
-                    RaisedButton(
-                        color: Colors.redAccent,
-                        textColor: Colors.white,
-                        onPressed: () {
-                          final snackBarOnSuccess = SnackBar(
-                              content: Text('Guardado correctamente...!'));
-                          final snackBar =
-                              SnackBar(content: Text('Procesando datos...'));
-                          _scaffoldKey.currentState.showSnackBar(snackBar);
-                        },
-                        child: _buttonSave()),
-                ])))));
+                    margin: EdgeInsets.all(20.0),
+                    elevation: 10.0,
+                    child: Column(children: <Widget>[
+                      Icon(Icons.group,
+                          size: 150.0,
+                          color: Theme.of(context).primaryColorLight),
+                      Container(
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.all(15.0),
+                          child: Column(children: <Widget>[
+                            Text(voluntierMessage,
+                                textAlign: TextAlign.justify),
+                            Padding(
+                                padding: EdgeInsets.only(top: 10.0),
+                                child: Text(
+                                  "¡Anímate y únete a la red de voluntarios!",
+                                  style: TextStyle(fontSize: 20.0),
+                                  textAlign: TextAlign.center,
+                                ))
+                          ])),
+                      Padding(
+                        padding: EdgeInsets.all(20),
+                        child: RaisedButton(
+                            color: Colors.redAccent,
+                            textColor: Colors.white,
+                            padding: EdgeInsets.all(10),
+                            onPressed: () {
+                              final snackBar = SnackBar(
+                                  content: Text('Procesando datos...'));
+                              _scaffoldKey.currentState.showSnackBar(snackBar);
+                              _enroll();
+                            },
+                            child: _buttonSave()),
+                      )
+                    ])))));
+  }
+
+  _enroll() {
+    String userUID;
+    String userName;
+    String email;
+    final snackBarOnSuccess =
+        SnackBar(content: Text('Bienvenido al programa! Redirigiendo...'));
+    final snackBarOnFailure =
+        SnackBar(content: Text('Tu ya eres parte del programa'));
+
+    FirebaseAuth.instance.currentUser().then((value) => {
+          userUID = value.uid,
+          userName = value.displayName,
+          email = value.email,
+          Firestore.instance
+              .document('Voluntiers/' + value.uid)
+              .get()
+              .then((value) => {
+                    print(value),
+                    if (!value.exists)
+                      {
+                        Firestore.instance
+                            .collection('Voluntiers')
+                            .document(userUID)
+                            .setData({
+                          'enrollTime': DateTime.now(),
+                          'attended': 0,
+                          'displayName': userName,
+                          'emailAddress': email
+                        }).whenComplete(() => {
+                                  _scaffoldKey.currentState
+                                      .showSnackBar(snackBarOnSuccess),
+                                  Timer(Duration(seconds: 5),
+                                      () => {Navigator.pop(context)})
+                                })
+                      }
+                    else
+                      {
+                        _scaffoldKey.currentState
+                            .showSnackBar(snackBarOnFailure)
+                      }
+                  })
+              .catchError((error) => {print(error)})
+        });
   }
 
   _buttonSave() {
@@ -50,7 +118,10 @@ class _VoluntierEnrollmentPageState extends State<VoluntierEnrollmentPage> {
     if (this.isProcessingData) {
       contentButton = CircularProgressIndicator();
     } else {
-      contentButton = Text('Enviar');
+      contentButton = Text(
+        'Unirse al voluntariado',
+        style: TextStyle(fontSize: 20.0),
+      );
     }
     return contentButton;
   }
