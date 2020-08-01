9fc35df94be04a90d99d17fa33edd45f6d705268
begandroide <benjamin.gautier.14@sansano.usm.cl>
2020-08-01 19:51:06 -0400

    :chart_with_upwards_trend: Se modifica card de estadísticas, se muestran con un Expanded los casos cerrados y abiertos de la app; falta conectar con Cloud, pero detalle a cubrir en otra iteración

--- sarshelp/lib/screens/home_content.dart
+++ sarshelp/lib/screens/home_content.dart
@@ -48,6 +48,7 @@ class _HomeContentState extends State<HomeContent> {
 
   Widget _getVoluntiersCard() {
     return Card(
+      elevation: 10.0,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
@@ -86,17 +87,108 @@ class _HomeContentState extends State<HomeContent> {
 
   Widget _getGeneralStatistics() {
     return SizedBox(
-        height: 400,
+        height: 380,
         child: Card(
+          elevation: 10.0,
           child: Column(
             mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               const ListTile(
                 leading: Icon(Icons.trending_up),
                 title: Text('Estadísticas'),
                 subtitle: Text('Resumen del estado actual de las alertas'),
                 isThreeLine: true,
-              )
+              ),
+              IntrinsicHeight(
+                  child: Padding(
+                      padding: EdgeInsets.only(bottom: 20.0),
+                      child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Container(
+                              height: 260,
+                              width: 170,
+                              padding: EdgeInsets.all(8),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  const Text(
+                                    "10",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 80.0),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      "Casos abiertos",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Theme.of(context).accentColor,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Icon(
+                                      Icons.warning,
+                                      size: 100,
+                                      color: Theme.of(context).accentColor,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  border: Border.all(color: Colors.grey[200])),
+                            ),
+                            Container(
+                              height: 260,
+                              width: 170,
+                              padding: EdgeInsets.all(8),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  const Text(
+                                    "0",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 80.0),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      "Casos cerrados",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.green[700],
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Icon(
+                                      Icons.check_circle_outline,
+                                      size: 100,
+                                      color: Colors.green[700],
+                                    ),
+                                  )
+                                ],
+                              ),
+                              decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                  border: Border.all(color: Colors.grey[200])),
+                            ),
+                          ])))
             ],
           ),
         ));
